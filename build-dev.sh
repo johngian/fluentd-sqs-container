0208932cf227a73208bd2defa026cc839baf8953
andrewkrug <andrewkrug@gmail.com>
2017-05-03 08:54:35 -0700

    update dev build script

--- build-dev.sh
+++ build-dev.sh
@@ -1,3 +1,5 @@
+aws ecr get-login | bash
+cd ansible
 ansible-container build
 docker tag ansible-fluentd:latest 656532927350.dkr.ecr.us-west-2.amazonaws.com/sqs-fluentd:latest 
 docker push 656532927350.dkr.ecr.us-west-2.amazonaws.com/sqs-fluentd:latest
